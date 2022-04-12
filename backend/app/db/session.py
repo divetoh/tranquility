52b0fd1fa37aa3877b8ed8a7031a2bfa2e11af89
Sergey Shirobokih <divetoh@yandex.ru>
2022-04-13 00:24:49 +0300

    SQL session timeout fix

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -2,4 +2,4 @@ from sqlalchemy.ext.asyncio import create_async_engine
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, pool_recycle=3600)
